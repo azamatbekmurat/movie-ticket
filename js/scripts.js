db98e096ed306387914c093ccd106ac64e733c61
Azamat Bekmuratov and Alex Noble <alex@>
2018-04-03 11:04:53 -0700

    add basic structure of constractor and prototype functions in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,48 @@
 
-function generatePrice(movieName) {
+function Ticket(movieName, time) {
   this.movieName = movieName;
   this.movieTime = time;
+  // this.movieTime = time;
+  // this.discount = discount;
+}
+
+Ticket.prototype.getPrice = function()  {
+  var price = [];
+  if (this.movieName==="acrimony"){
+    if (this.movieTime < 6) {
+      price.push("10$");
+    } else {
+      price.push("20$");
+    }
+  } else if (this.movieName==="midSun$") {
+    if (this.movieTime < 6) {
+      price.push("30$");
+    } else {
+      price.push("40$");
+    }
+  } else {
+    return "wrong input";
+  }
+  return price;
+}
+
+Ticket.prototype.ticketPrint = function() {
+  return this.movieName + ", " + this.movieTime;
 }
 
 $(document).ready(function() {
   $("form#movie-ticket").submit(function(event) {
     event.preventDefault();
 
-    var checkValue = $("input:radio[name=exampleRadios]:checked").val();
-    console.log(checkValue);
+    var inputValue = $("input:radio[name=exampleRadios]:checked").val();
+    var movieName = inputValue.slice(0,-1);
+    var movieTime = inputValue.slice(-1);
 
+    var newTicket = new Ticket(movieName,movieTime);
+
+    var ticketPrice = newTicket.getPrice();
+
+    $("ul#ticket-output").append("<li><span class='ticket'>" + newTicket.ticketPrint() + ", " + ticketPrice +  "</span></li>");
 
   });
 });
